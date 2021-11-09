9c8ee90c92c9ad797f8bc1ff2ac9ab779ae815bb
Veronika <verona.binko@gmail.com>
2021-11-09 14:55:31 +0300

    Feat: add useState and UseRef in SimpleInput.js, both variants for submitting of form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,30 @@
+import { useRef, useState} from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmisiionHandler = event => {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    
+    // nameInputRef.current.value = ''; Так лучше не делать. Манипулировать Dom.
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmisiionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
